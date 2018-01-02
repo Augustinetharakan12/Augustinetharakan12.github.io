9f40569fc308c3b8a4b8fc2e149db216d5bb7360
Augustinetharakan12 <augustinetharakan12@gmail.com>
2018-01-02 19:07:57 +0530

    new

---
+++ static/script.js
@@ -0,0 +1,10 @@
+document.getElementById('addpost').addEventListener('submit',funct);
+
+function funct(){
+    let title=document.getElementById('name').value;
+    let title=document.getElementById('name').value;
+    let title=document.getElementById('name').value;
+    let title=document.getElementById('name').value;
+    let title=document.getElementById('name').value;
+    document.getElementById('new').innerHTML='working'
+}
